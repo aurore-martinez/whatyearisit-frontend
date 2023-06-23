21b63b8f6b50970c8959d3720eb95324b106b421
Aurore Martinez <aurore.jp.martinez@gmail.com>
2023-06-23 12:01:05 +0200

    firstcommit

---
+++ script.js
@@ -0,0 +1,7 @@
+	fetch('http://whatyearisit-backend-five-eta.vercel.app/year')
+    .then(response => response.json())
+	.then(data => {
+console.log(data);
+const {year} = data;
+document.querySelector("#year").textContent =year
+    })
