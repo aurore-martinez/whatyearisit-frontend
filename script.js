7335d0755a03f4942a11a7a37e8e89f7d48cb69e
Aurore Martinez <aurore.jp.martinez@gmail.com>
2023-06-23 15:21:19 +0200

    front vercel

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-	fetch('http://whatyearisit-backend-five-eta.vercel.app/year')
+	fetch('https://whatyearisit-backend-five-eta.vercel.app/year')
     .then(response => response.json())
 	.then(data => {
 console.log(data);
